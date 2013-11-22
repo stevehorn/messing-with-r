de4f7ae899c5227f38e9c393810397ff38ca3de5
Steve Horn <steve@stevehorn.cc>
2013-11-21 23:05:10 -0500

    pareto chart

--- r/ascii_charts.r
+++ r/ascii_charts.r
@@ -2,3 +2,9 @@ library(aplpack)
 #Visualization meant to give you an idea of how many
 #occurences happened in each range
 stem.leaf(UKDriverDeaths, depth = FALSE)
+
+Tbl <- table(state.division)
+Tbl
+
+Tbl/sum(Tbl)
+prop.table(Tbl)
--- r/charts.r
+++ r/charts.r
@@ -9,3 +9,14 @@ hist(precip, main = "")
 plot(LakeHuron, type = "h")
 plot(LakeHuron, type = "p")
 
+barplot(table(state.region), cex.names = 0.5)
+barplot(prop.table(table(state.region)), cex.names = 0.5)
+
+
+library(qcc) 
+# series of value w/ mean of 10 with a little random noise added in
+x <- rep(10, 100) + rnorm(100)
+# a test series w/ a mean of 11
+new.x <- rep(11, 15) + rnorm(15)
+# qcc will flag the new points
+qcc(x, newdata=new.x, type="xbar.one")
