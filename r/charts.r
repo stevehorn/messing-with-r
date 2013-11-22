e06e1c00cd45ecbb7aac5ec3562570b0837b83cb
Steve Horn <steve@stevehorn.cc>
2013-11-21 20:49:40 -0500

    Remove par statement

--- r/charts.r
+++ r/charts.r
@@ -2,8 +2,5 @@
 #Run using $ rscript charts.r
 #PDF file will be output in same directory
 stripchart(precip, xlab = "rainfall")
-par(ask=TRUE)
 stripchart(rivers, method = "jitter", xlab = "length")
-par(ask=TRUE)
 stripchart(discoveries, method = "stack", xlab = "number")
-par(ask=TRUE)
